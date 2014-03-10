87368b7f7be67a3a91b7962ae8da57fb9b13147e
Katie Kuksenok <kuksenok@cs.washington.edu>
2014-03-10 11:23:44 -0700

    More data processing stuff

--- dbproc/src/dbproc/Dbproc.java
+++ dbproc/src/dbproc/Dbproc.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dbproc;
 
 import java.io.BufferedReader;
@@ -32,14 +27,15 @@ public class Dbproc {
         Trip.init(conn);
         Stop.init(conn);
 
-        String[] folders = new String[]{"../clean data/december 2011", "../clean data/march 2012"};
+       String[] folders = new String[]{"../clean data/march 2012", "../clean data/december 2011"};
+       // String[] folders = new String[]{"../clean data/june-2012"};
         for (String folder : folders) {
-            Map<Integer, Trip> trips = getRouteInfo(folder);
+            Map<String, Trip> trips = getRouteInfo(folder);
             List<String> datafiles = getDataFiles(folder);
             System.out.println("Read " + trips.size() + " trips. Going to read " + datafiles.size() + " datafiles");
             for (String file : datafiles) {
                 int[] reads = pushStops(folder + "/" + file, trips);
-                System.out.println(reads[1] + " successes, " + reads[0] + " failures");
+                System.out.println(reads[1] + " successes, " + reads[0] + " failures and " + reads[2] + " blank locations");
             }
         }
     }
@@ -57,24 +53,24 @@ public class Dbproc {
         return list;
     }
 
-    public static int[] pushStops(String path, Map<Integer, Trip> trips) throws FileNotFoundException, IOException {
+    public static int[] pushStops(String path, Map<String, Trip> trips) throws FileNotFoundException, IOException {
         FileReader fr = new FileReader(path);
         BufferedReader tr = new BufferedReader(fr);
-        int[] read = new int[2];
+        int[] read = new int[3];
         String line = tr.readLine(); // header row: locationLat	locationLon	scheduleDeviation	time	trip_id	vehicle_agencyId	vehicle_id
         while ((line = tr.readLine()) != null) {
             String[] parts = line.split("\t");
-            if (Integer.parseInt(parts[5]) != 1) {
-                read[0]++;
-                continue;
-            }
+           
             Stop s = new Stop();
             try {
-                s.lat = Double.parseDouble(parts[0]);
-                s.lon = Double.parseDouble(parts[1]);
-                s.deviation = Integer.parseInt(parts[2]);
+                s.lat = (parts[0].equals("NULL")) ? 0 : Double.parseDouble(parts[0]);
+                s.lon = (parts[1].equals("NULL")) ? 0 : Double.parseDouble(parts[1]);
+                if (s.lat == 0 || s.lon == 0) {
+                    read[2]++;
+                }
+                s.deviation = (parts[0].equals("NULL")) ? 0 : Integer.parseInt(parts[2]);
                 s.time = Long.parseLong(parts[3]);
-                Trip t = trips.get(Integer.parseInt(parts[4]));
+                Trip t = trips.get(parts[4]);
                 if (t == null || t.fk_trip_id == -1) {
                     read[0]++;
                     continue;
@@ -86,7 +82,7 @@ public class Dbproc {
                         System.out.println("write fail : " + line);
                         read[0]++;
                     } else {
-                        read[1] ++;
+                        read[1]++;
                     }
                 } catch (SQLException e) {
                     System.out.println("sql fail : " + line);
@@ -103,8 +99,8 @@ public class Dbproc {
     /**
      * @return gtfs_trip_id->Trip
      */
-    public static HashMap<Integer, Trip> getRouteInfo(String folder) throws FileNotFoundException, IOException {
-        HashMap<Integer, Trip> trips = new HashMap<>();
+    public static HashMap<String, Trip> getRouteInfo(String folder) throws FileNotFoundException, IOException {
+        HashMap<String, Trip> trips = new HashMap<>();
         FileReader fr = new FileReader(folder + "/routes.txt");
         BufferedReader tr = new BufferedReader(fr);
         String line = tr.readLine(); // header row: route_short_name	route_id
@@ -112,7 +108,7 @@ public class Dbproc {
             String[] parts = line.split("\t");
             Trip t = new Trip();
             t.short_name = parts[0];
-            t.route_id = Integer.parseInt(parts[1]);
+            t.route_id = parts[1];
             trips.put(t.route_id, t);
         }
         fr.close();
@@ -121,18 +117,25 @@ public class Dbproc {
         line = tr.readLine(); // route_id	trip_headsign	trip_id
         while ((line = tr.readLine()) != null) {
             String[] parts = line.split("\t");
-            Trip t = trips.get(Integer.parseInt(parts[0]));
-            t.headsign = parts[1];
-            t.gtfs_trip_id = Integer.parseInt(parts[2]);
+            try{
+                Trip t = trips.get(parts[0]);
+                t.headsign = parts[1];
+                t.gtfs_trip_id = parts[2];
+            } catch(NullPointerException npe){
+                System.out.println(line);
+                npe.printStackTrace();
+            }
         }
         tr.close();
-        HashMap<Integer, Trip> result = new HashMap<>();
-        for (Integer i : trips.keySet()) {
+        HashMap<String, Trip> result = new HashMap<>();
+        for (String i : trips.keySet()) {
             Trip t = trips.get(i);
             try {
                 t.fk_trip_id = t.fetchFkTripId();
             } catch (SQLException e) {
-                System.out.println("Unable to resolve " + t.gtfs_trip_id + " ->> skipping");
+                System.out.println("Unable to resolve " + t.gtfs_trip_id + " // "+t);
+                
+                e.printStackTrace();
             }
             result.put(t.gtfs_trip_id, t);
         }
@@ -150,17 +153,17 @@ class Trip {
         writer = conn.prepareStatement("insert into trip values (default, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
         reader = conn.prepareStatement("select id from trip where gtfs_trip_id=?");
     }
-    int gtfs_trip_id;
-    int route_id;
+    String gtfs_trip_id;
+    String route_id;
     String headsign;
     String short_name;
     int fk_trip_id = -1;
 
     public int write() throws SQLException {
-        writer.setInt(1, route_id);
+        writer.setString(1, route_id);
         writer.setString(2, headsign);
         writer.setString(3, short_name);
-        writer.setInt(4, gtfs_trip_id);
+        writer.setString(4, gtfs_trip_id);
         writer.executeUpdate();
         ResultSet res = writer.getGeneratedKeys();
         while (res.next()) {
@@ -170,7 +173,7 @@ class Trip {
     }
 
     public int fetchFkTripId() throws SQLException {
-        reader.setInt(1, gtfs_trip_id);
+        reader.setString(1, gtfs_trip_id);
         ResultSet res = reader.executeQuery();
         while (res.next()) {
             return res.getInt(1);
@@ -194,9 +197,9 @@ class Stop {
         ps = conn.prepareStatement("insert into stop values (default, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
     }
     int fk_trip_id;
-    int gtfs_trip_id;
+    int trip_id;
     int vehicle_id;
-    double lat, lon;
+    Double lat, lon;
     int deviation;
     long time;
 
@@ -205,15 +208,15 @@ class Stop {
         ps.setDouble(2, lon);
         ps.setInt(3, deviation);
         ps.setTimestamp(4, new Timestamp(time));
-        ps.setInt(5, fk_trip_id);
-        ps.setInt(6, vehicle_id);
+        ps.setInt(5, vehicle_id);
 
         Date date = new Date(time);
         Calendar calendar = GregorianCalendar.getInstance();
         calendar.setTime(date);
-        ps.setString(7, calendar.get(Calendar.MONTH) + " " + calendar.get(Calendar.DAY_OF_MONTH) + " " + calendar.get(Calendar.YEAR));
-        ps.setInt(8, calendar.get(Calendar.HOUR_OF_DAY));
-        ps.setInt(9, calendar.get(Calendar.MINUTE));
+        ps.setString(6, calendar.get(Calendar.MONTH) + " " + calendar.get(Calendar.DAY_OF_MONTH) + " " + calendar.get(Calendar.YEAR));
+        ps.setInt(7, calendar.get(Calendar.HOUR_OF_DAY));
+        ps.setInt(8, calendar.get(Calendar.MINUTE));
+        ps.setInt(9, fk_trip_id);
         return ps.executeUpdate();
     }
 }

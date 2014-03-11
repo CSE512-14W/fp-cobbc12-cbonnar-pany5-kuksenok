dcc93c73013a5beceddc124e6c3146d7c4ff454b
Katie Kuksenok <kuksenok@cs.washington.edu>
2014-03-10 21:38:27 -0700

    Congestion API

--- api/congestion.php
+++ api/congestion.php
@@ -1,7 +1,7 @@
 <?php
 
-$before = isset($_GET['before'])?$_GET['before']:null;
-$after = isset($_GET['after'])?$_GET['after']:null;
+$before = isset($_GET['before'])?$_GET['before']:"23:59";
+$after = isset($_GET['after'])?$_GET['after']:"00:00";
 $days = isset($_GET['days'])?$_GET['days']:null;
 if ($days==null && isset($_GET['cut'])){
 	$days = $_GET['cut']=="pre"?"2011-12-17,2011-12-21,2011-12-18,2011-12-16":"2012-03-02,2012-03-03,2012-03-04,2012-03-07";
@@ -13,37 +13,40 @@ $days = explode(",", $days);
 
 foreach ($days as $key => $value) {
 	if(!in_array($value, $valid)){
-		unset($filter[$key]);
+		unset($days[$key]);
 	}
 }
 
-$con = mysqli_connect("localhost","root","","bus2");
+if (empty($days)){
+	$days = $valid;
+}
 
-if (mysqli_connect_errno($con))
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
+$mysqli = new mysqli("localhost","root","","bus2");
 
-$q = "SELECT lat, lon, count(*) FROM schedule, stops, route, trips ".
-	 "WHERE schedule.stop_id = stops.stop_id AND trips.trip_id = schedule.trip_id AND route.route_short_name IN (\"".
-	 	implode("\",\"",$filter) ."\") AND trips.trip_id=route.route_id GROUP BY stops.stop_id";
+$q = "SELECT lat, lon, AVG(deviation), COUNT(deviation) FROM deviation ".
+	 "WHERE oba_day IN (\"". implode("\",\"",$days) ."\") GROUP BY lat, lon";
 
-echo $q;
+// Extract result set and loop rows
+$result = $mysqli->query($q);
+while ($row = $result->fetch_assoc()) {
+   print_r($row);
+}
 
-print_r(mysqli_query($con, $q));
-
-mysqli_close($con);
+$mysqli->close();
 ?>
 
-var geojsonFeature = {
-    "type": "Feature",
-    "properties": {
-        "name": "Coors Field",
-        "amenity": "Baseball Stadium",
-        "popupContent": "This is where the Rockies play!"
-    },
-    "geometry": {
-        "type": "Point",
-        "coordinates": [-104.99404, 39.75621]
-    }
-};
+var stops = [{
+   "type": "Feature",
+   "properties": {"avg_deviation": "-1200", "stop_name": "40th and Stone Way N"},
+   "geometry": {
+       "type": "Point",
+       "coordinates":  [-104.05, 48.99],
+   }
+}, {
+   "type": "Feature",
+   "properties": {"avg_deviation": "0", "stop_name": "Stevens and Benton"},
+   "geometry": {
+       "type": "Point",
+       "coordinates": [-109.05, 41.00],
+   }
+}];

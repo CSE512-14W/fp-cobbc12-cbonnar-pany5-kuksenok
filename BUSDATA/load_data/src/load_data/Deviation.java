3d131fff7434b446d058e130953a6bd999c584c7
Katie Kuksenok <kuksenok@cs.washington.edu>
2014-03-10 22:24:48 -0700

    Fake congestion API up

--- BUSDATA/load_data/src/load_data/Deviation.java
+++ BUSDATA/load_data/src/load_data/Deviation.java
@@ -47,7 +47,7 @@ class DeviationDataSources {
         Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/bus2?user=root&password=");
         PreparedStatement ps = conn.prepareStatement("SELECT trip_id, lat, lon FROM schedule, stops WHERE AND trips.route_id=route.route_id AND trips.gtfs_data=route.gtfs_data AND route.gtfs_data=?");
         ps.setString(1, gtfs);
-        
+
         PreparedStatement writeObaTuple = conn.prepareCall("INSERT INTO deviation SET lat=?, lon=?, deviation=?, time=?, hh=?, mm=?, oba_day=?, trip_id=?, vehicle_id=?");
         for (String oba_file : oba) {
             FileReader fr = new FileReader("../DATA/" + oba_file + " oba.txt");
@@ -59,27 +59,32 @@ class DeviationDataSources {
              */
             while ((line = tr.readLine()) != null) {
                 String[] parts = line.split("\t");
-                try{
-                double lat = Double.parseDouble(parts[0]);
-                double lon = Double.parseDouble(parts[1]);
-                int deviation = Integer.parseInt(parts[2]);
-                long time = Long.parseLong(parts[3]);
-                int trip_id = Integer.parseInt(parts[4]);
-                int vehicle_id = Integer.parseInt(parts[6]);
-                Date date = new Date(time);
-                Calendar calendar = GregorianCalendar.getInstance();
-                calendar.setTime(date);
-                writeObaTuple.setDouble(1, lat);
-                writeObaTuple.setDouble(2, lon);
-                writeObaTuple.setInt(3, deviation);
-                writeObaTuple.setTimestamp(4, new Timestamp(time));
-                writeObaTuple.setInt(5, calendar.get(Calendar.HOUR_OF_DAY));
-                writeObaTuple.setInt(6, calendar.get(Calendar.MINUTE));
-                writeObaTuple.setString(7, oba_file);
-                writeObaTuple.setInt(8, trip_id);
-                writeObaTuple.setInt(9, vehicle_id);
-                writeObaTuple.executeUpdate();
-                } catch(Exception e){
+                try {
+                    double lat = Double.parseDouble(parts[0]);
+                    double lon = Double.parseDouble(parts[1]);
+                    int deviation = Integer.parseInt(parts[2]);
+                    long time = Long.parseLong(parts[3]);
+                    int trip_id = -1;
+                    int vehicle_id = -1;
+                    try {
+                        trip_id = Integer.parseInt(parts[4]);
+                        vehicle_id = Integer.parseInt(parts[6]);
+                    } catch (Exception e) {
+                    }
+                    Date date = new Date(time);
+                    Calendar calendar = GregorianCalendar.getInstance();
+                    calendar.setTime(date);
+                    writeObaTuple.setDouble(1, lat);
+                    writeObaTuple.setDouble(2, lon);
+                    writeObaTuple.setInt(3, deviation);
+                    writeObaTuple.setTimestamp(4, new Timestamp(time));
+                    writeObaTuple.setInt(5, calendar.get(Calendar.HOUR_OF_DAY));
+                    writeObaTuple.setInt(6, calendar.get(Calendar.MINUTE));
+                    writeObaTuple.setString(7, oba_file);
+                    writeObaTuple.setInt(8, trip_id);
+                    writeObaTuple.setInt(9, vehicle_id);
+                    writeObaTuple.executeUpdate();
+                } catch (Exception e) {
                     //e.printStackTrace();;
                 }
             }
--- api/congestion.php
+++ api/congestion.php
@@ -3,9 +3,17 @@
 $before = isset($_GET['before'])?$_GET['before']:"23:59";
 $after = isset($_GET['after'])?$_GET['after']:"00:00";
 $days = isset($_GET['days'])?$_GET['days']:null;
+
 if ($days==null && isset($_GET['cut'])){
 	$days = $_GET['cut']=="pre"?"2011-12-17,2011-12-21,2011-12-18,2011-12-16":"2012-03-02,2012-03-03,2012-03-04,2012-03-07";
 }
+
+if (isset($_GET['cut']) && $_GET['cut']=="pre"){
+   include("cong_pre.htm");
+} else {
+   include("cong_post.htm");
+}
+die();
 
 $valid = explode(",", "2011-12-17,2011-12-21,2011-12-18,2011-12-16,2012-03-02,2012-03-03,2012-03-04,2012-03-07");
 
@@ -24,25 +32,26 @@ if (empty($days)){
 $mysqli = new mysqli("localhost","root","","bus2");
 
 $q = "SELECT lat, lon, AVG(deviation) avg, COUNT(deviation) ct FROM deviation ".
-	 "WHERE oba_day IN (\"". implode("\",\"",$days) ."\") GROUP BY lat, lon";
+	 "WHERE oba_day IN (\"". implode("\",\"",$days) ."\") GROUP BY lat, lon ORDER BY RAND()";
 
 // Extract result set and loop rows
 $result = $mysqli->query($q);
+$ct = 0;
 echo "[{\n";
 while ($row = $result->fetch_assoc()) {
-   $row["lat"];
-   $row["lon"];
-   $row["avg"];
-   $row["ct"];
    echo "    \"type\": \"Feature\",\n";
    echo "    \"properties\": {\"avg_deviation\": \"" . $row["avg"] . "\", \"data_points\": \"" . $row["ct"] . "\"},\n";
    echo "    \"geometry\": {\n";
-   echo "        \"coordinates\": [".$avg["lat"].",".$avg["lon"]."],\n";
+   echo "        \"coordinates\": [".$row["lat"].",".$row["lon"]."],\n";
    echo "        \"type\": \"Point\",\n";
    echo "    }\n";
    echo "}, {\n";
+   $ct++;
+   if($ct>100){
+      break;
+   }
 }
-echo "}];";
+echo "}]";
 
 $mysqli->close();
 ?>

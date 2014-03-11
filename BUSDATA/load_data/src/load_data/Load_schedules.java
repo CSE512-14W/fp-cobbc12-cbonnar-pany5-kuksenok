08a4f23b0301bc6c94a93be6fadbd07cfc8f5dbc
Katie Kuksenok <kuksenok@cs.washington.edu>
2014-03-10 17:30:33 -0700

    Cleanup/rename

--- BUSDATA/load_data/src/load_data/Load_schedules.java
+++ BUSDATA/load_data/src/load_data/Load_schedules.java
@@ -12,7 +12,7 @@ import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.HashSet;
 
-public class Beautifier {
+public class Load_schedules {
 
     public static void main(String[] args) throws SQLException, IOException {
         String[] routes = new String[]{"31", "32", "16", "26", "28", "40"};
